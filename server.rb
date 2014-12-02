b849a6c1a0d9537388e381ac561ecf5037cbc995
ablease <alexblease@gmail.com>
2014-12-02 12:16:55 +0000

    Greated simpky sinatra app

--- server.rb
+++ server.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 
 class GithubProfile < Sinatra::Base
-  
+
+set :views, Proc.new { File.join(root, 'views') }
+
   get '/' do
-    response['Access-Control-Alex-Origin']= '*'
+    response['Access-Control-Alex-Origin'] = '*'
     Time.now.to_s
+    erb :index
   end
 
 end
