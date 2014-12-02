20c9db863c560492585327b03a534a92df80ca00
Shan Hasan <shan119@hotmail.co.uk>
2014-12-02 16:29:47 +0000

    updated index file to allow different users to be loaded

--- server.rb
+++ server.rb
@@ -9,8 +9,9 @@ set :views, Proc.new { File.join(root, 'views') }
 
   get '/' do
     response['Access-Control-Allow-Origin'] = '*'
-    sleep 2
+    # sleep 2
     Time.now.to_s
+    erb :index
   end
 
 end
