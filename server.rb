7e9792d1505f5c7d9c881c6fd4e0a3a84f3d68fb
ablease <alexblease@gmail.com>
2014-12-02 16:41:23 +0000

    Moved css to public folder

--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ Dotenv.load
 class GithubProfile < Sinatra::Base
 
 set :views, Proc.new { File.join(root, 'views') }
+set :public_folder, Proc.new { File.join(root, 'public') }
 
   get '/' do
     response['Access-Control-Allow-Origin'] = '*'
